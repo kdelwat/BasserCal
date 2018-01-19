3410137a58804c3c9c8692a84c274abc656ddc37
Cadel Watson <cadel@cadelwatson.com>
2018-01-20 10:34:47 +1100

    Add events database

---
+++ persistence/db.js
@@ -0,0 +1,43 @@
+var low = require('lowdb');
+var FileSync = require('lowdb/adapters/FileSync');
+var moment = require('moment');
+
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+
+db
+  .defaults({
+    events: [
+      {
+        id: 1,
+        name: 'Bar chase',
+        date: moment('2018-01-22'),
+        portfolio: 'social',
+        description: 'A nice event'
+      },
+      {
+        id: 2,
+        name: 'Mr. Burns play',
+        date: moment('2018-01-22'),
+        portfolio: 'cultural',
+        description: 'A nicer event'
+      },
+      {
+        id: 3,
+        name: 'Clean up Australia Day',
+        date: moment('2018-01-21'),
+        portfolio: 'communities',
+        description: 'A nicest event'
+      },
+      {
+        id: 4,
+        name: 'MI6 Party',
+        date: moment('2018-01-20'),
+        portfolio: 'social',
+        description: 'A nicererer event'
+      }
+    ]
+  })
+  .write();
+
+module.exports = db;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
+var db = require('../persistence/db');
+
+const getWeekEvents = () => {
+  const startDate = moment();
+
+  let events = [];
+  for (let i = 0; i <= 6; i++) {
+    const day = {
+      date: startDate.clone().add(i, 'days')
+    };
+
+    day.events = db
+      .getState()
+      .events.filter(event => moment(event.date).isSame(day.date, 'day'));
+
+    events.push(day);
+  }
+
+  return events;
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const events = getWeekEvents();
+  console.log(events);
+  res.render('index', { events: events });
 });
 
 module.exports = router;
