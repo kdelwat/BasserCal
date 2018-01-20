e4e3736aa6ffcddce2f445f8035f85a8e0aaec27
Cadel Watson <cadel@cadelwatson.com>
2018-01-20 11:26:03 +1100

    Add editing events

--- routes/events.js
+++ routes/events.js
@@ -14,4 +14,38 @@ router.get('/:id', function(req, res, next) {
   res.render('details', { event: event });
 });
 
+router.get('/:id/edit', function(req, res, next) {
+  const id = parseInt(req.params.id, 10);
+
+  const event = db
+    .get('events')
+    .find({ id: id })
+    .value();
+
+  res.render('edit', {
+    event: event,
+    dateValue: moment(event.date).format('YYYY-MM-DD'),
+    timeValue: moment(event.date).format('HH:mm')
+  });
+});
+
+router.post('/:id/edit', function(req, res, next) {
+  const id = parseInt(req.params.id, 10);
+  const event = {
+    id: id,
+    name: req.body.name,
+    description: req.body.description,
+    date: moment(req.body.date + ' ' + req.body.time)
+  };
+  console.log(event);
+  db
+    .get('events')
+    .find({ id: id })
+    .assign(event)
+    .write();
+
+  res.status(200);
+  res.send('Successfully edited');
+});
+
 module.exports = router;
